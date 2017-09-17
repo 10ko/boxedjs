e98e4b48f026df1c26c04dca216cc0fb5c6e70f2
Emanuele Libralato <e.libralato@gmail.com>
2017-09-18 00:02:06 +0200

    Installed eslint and fixed linting errors

--- index.js
+++ index.js
@@ -3,13 +3,14 @@
 const watch = require('node-watch');
 const program = require('commander');
 const boxed = require('./lib/boxed');
-const { isFileValidForWatch } = require('./lib/utils');
+const {isFileValidForWatch} = require('./lib/utils');
 const packagejson = require('./package.json');
 
 
 program
   .version(packagejson.version)
-  .option('-w, --watch', 'Watch functionality. It will rebuild the website whenever a file changes.')
+  .option('-w, --watch', 'Watch functionality. It will rebuild the website '
+    + 'whenever a file changes.')
   .option('-n, --new <name>', 'Create new project')
   .parse(process.argv);
 
@@ -20,7 +21,7 @@ if (program.new) {
 }
 
 if (program.watch) {
-  watch('.', { recursive: true, filter: isFileValidForWatch }, (evt, name) => {
+  watch('.', {recursive: true, filter: isFileValidForWatch}, (evt, name) => {
     console.log('%s changed.', name);
     boxed.compile();
   });
